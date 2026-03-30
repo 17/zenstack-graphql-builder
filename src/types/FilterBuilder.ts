af07f78c4949811727fe271dc30ca084e3f0ff92
Jase <unmric@gmail.com>
2026-03-30 17:24:44 +0800

    fix(FilterBuilder): 移除不存在的聚合过滤字段

--- src/types/FilterBuilder.ts
+++ src/types/FilterBuilder.ts
@@ -106,10 +106,7 @@ export class FilterBuilder {
                 startsWith: { type: GraphQLString },
                 endsWith: { type: GraphQLString },
                 mode: { type: QueryModeEnum },
-                not: { type: this.getFilter('String')! },
-                _count: { type: this.getFilter('Int')! },
-                _min: { type: this.getFilter('String')! },
-                _max: { type: this.getFilter('String')! },
+                not: { type: this.getFilter('String')! }
             }),
         });
         this.typeCache.set(name, filter);
@@ -133,12 +130,7 @@ export class FilterBuilder {
                 gt: { type: baseType },
                 gte: { type: baseType },
                 between: { type: new GraphQLList(new GraphQLNonNull(baseType)) },
-                not: { type: this.getFilter(typeName)! },
-                _count: { type: this.getFilter('Int')! },
-                _avg: { type: this.getFilter(typeName)! },
-                _sum: { type: this.getFilter(typeName)! },
-                _min: { type: this.getFilter(typeName)! },
-                _max: { type: this.getFilter(typeName)! },
+                not: { type: this.getFilter(typeName)! }
             }),
         });
         this.typeCache.set(name, filter);
@@ -180,10 +172,7 @@ export class FilterBuilder {
                 gt: { type: DateTimeScalar },
                 gte: { type: DateTimeScalar },
                 between: { type: new GraphQLList(new GraphQLNonNull(DateTimeScalar)) },
-                not: { type: this.getFilter('DateTime')! },
-                _count: { type: this.getFilter('Int')! },
-                _min: { type: this.getFilter('DateTime')! },
-                _max: { type: this.getFilter('DateTime')! },
+                not: { type: this.getFilter('DateTime')! }
             }),
         });
         this.typeCache.set(name, filter);
@@ -206,12 +195,7 @@ export class FilterBuilder {
                 gt: { type: BigIntScalar },
                 gte: { type: BigIntScalar },
                 between: { type: new GraphQLList(new GraphQLNonNull(BigIntScalar)) },
-                not: { type: this.getFilter('BigInt')! },
-                _count: { type: this.getFilter('Int')! },
-                _avg: { type: this.getFilter('BigInt')! },
-                _sum: { type: this.getFilter('BigInt')! },
-                _min: { type: this.getFilter('BigInt')! },
-                _max: { type: this.getFilter('BigInt')! },
+                not: { type: this.getFilter('BigInt')! }
             }),
         });
         this.typeCache.set(name, filter);
@@ -234,12 +218,7 @@ export class FilterBuilder {
                 gt: { type: DecimalScalar },
                 gte: { type: DecimalScalar },
                 between: { type: new GraphQLList(new GraphQLNonNull(DecimalScalar)) },
-                not: { type: this.getFilter('Decimal')! },
-                _count: { type: this.getFilter('Int')! },
-                _avg: { type: this.getFilter('Decimal')! },
-                _sum: { type: this.getFilter('Decimal')! },
-                _min: { type: this.getFilter('Decimal')! },
-                _max: { type: this.getFilter('Decimal')! },
+                not: { type: this.getFilter('Decimal')! }
             }),
         });
         this.typeCache.set(name, filter);
@@ -257,10 +236,7 @@ export class FilterBuilder {
                 equals: { type: BytesScalar },
                 in: { type: new GraphQLList(new GraphQLNonNull(BytesScalar)) },
                 notIn: { type: new GraphQLList(new GraphQLNonNull(BytesScalar)) },
-                not: { type: this.getFilter('Bytes')! },
-                _count: { type: this.getFilter('Int')! },
-                _min: { type: this.getFilter('Bytes')! },
-                _max: { type: this.getFilter('Bytes')! },
+                not: { type: this.getFilter('Bytes')! }
             }),
         });
         this.typeCache.set(name, filter);
@@ -302,10 +278,7 @@ export class FilterBuilder {
                 equals: { type: enumType },
                 in: { type: new GraphQLList(new GraphQLNonNull(enumType)) },
                 notIn: { type: new GraphQLList(new GraphQLNonNull(enumType)) },
-                not: { type: this.getFilter(enumType.name)! },
-                _count: { type: this.getFilter('Int')! },
-                _min: { type: this.getFilter(enumType.name)! },
-                _max: { type: this.getFilter(enumType.name)! },
+                not: { type: this.getFilter(enumType.name)! }
             }),
         });
         this.typeCache.set(name, filter);
